3b8db11f74bf718934ac618d2e947c70443c6620
unknown <tejtejas777k@gmail.com>
2025-01-02 20:34:46 +0530

    Changed entire system

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.kel2i.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://tejtejass827:<db_password>@devclust.kel2i.mongodb.net/?retryWrites=true&w=majority&appName=DevClust`;
 const client = new MongoClient(connectionString);
 
 let conn;
